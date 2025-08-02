5b32d86a9211e5e0ad270a92c9db3480e5aef2fe
Lautiguayan <lautyguayan1903@gmail.com>
2025-08-02 16:28:00 -0300

    V2 de mi proyecto personal de pagina web

---
+++ APP.js
@@ -0,0 +1,32 @@
+let b1=document.querySelector(`.GH`);
+
+b1.addEventListener(`click`, function () {
+
+    window.location.href = `https://github.com/LautiiGuayan`;
+
+
+}
+
+)
+
+let b2=document.querySelector(`.FB`);
+
+b2.addEventListener(`click`, function () {
+
+    window.location.href = `https://www.facebook.com/?locale=es_LA`;
+
+
+}
+
+)
+
+let b3=document.querySelector(`.IG`);
+
+b3.addEventListener(`click`, function () {
+
+    window.location.href = `https://www.instagram.com/`;
+
+
+}
+
+)
